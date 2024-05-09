4897568c7151dd04a5d075ce0e1de3957f27cd40
yoshikawa risa <yoshikawarisa@yoshikawanonotobukkukonpyuta.local>
2024-05-09 16:13:26 +0900

    “投稿ajaxまで”

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,16 +11,6 @@
 
 <body>
     <h1><a href="{{ route('posts.index') }}">Kisiko News🍻</a></h1>
-    <form action="{{ route('posts.store') }}" method="POST">
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-                </ul>
-            </div>
-        @endif
 
     <form id="postForm">
         @csrf
@@ -34,50 +24,7 @@
         <div id="messageError"></div>
 
         <button type="submit">投稿する</button>
-        </form>
-
-        <script>
-
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-
-            $(document).ready(function() {
-            $('#postForm').submit(function(e) {
-            e.preventDefault(); // デフォルトのフォーム送信を停止
-            if (confirm('マジ投稿しちゃう？')) {
-            $.ajax({
-                type: 'POST',
-                url: '{{ route('posts.store') }}',
-                data: $(this).serialize(),
-                success: function(response) {
-                    $('#title').val('');
-                    $('#message').val('');
-                    $('#titleError').empty();
-                    $('#messageError').empty();
-                }
-            });
-            } else {
-                return false;
-            }
-            });
-                // 投稿データから詳細ページへのリンクを生成
-                var newPostHtml = '<h3><a href="' + '/posts/' + response.post.id + '">タイトル：' + response.post.title +
-    '</a></h3><p>内容：' + response.post.message + '</p>';
-                $('#newsList').prepend(newPostHtml);
-            },
-
-            error: function(xhr) {
-                var errors = xhr.responseJSON.errors;
-                $('#titleError').text(errors.title && errors.title[0]);
-                $('#messageError').text(errors.message && errors.message[0]);
-            }
-        });
-
-        </script>
-
+    </form>
     <br><br>
     <h2>ニュース一覧</h2>
     <div id="newsList">
@@ -86,6 +33,41 @@
             <p>内容：{{ $post->message }}</p>
         @endforeach
     </div>
-</body>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
+        $(document).ready(function() {
+            $('#postForm').submit(function(e) {
+            e.preventDefault(); // デフォルトのフォーム送信を停止
+            if (confirm('マジ投稿しちゃう？')) {
+                $.ajax({
+                    type: 'POST',
+                    url: '{{ route('posts.store') }}',
+                    data: $(this).serialize(),
+                    success: function(response) {
+                        $('#title').val('');
+                        $('#message').val('');
+                        $('#titleError').empty();
+                        $('#messageError').empty();
+                        var newPostHtml = '<h3><a href="' + '/posts/' + response.post.id + '">タイトル：' + response.post.title +'</a></h3><p>内容：' + response.post.message + '</p>';
+                        $('#newsList').prepend(newPostHtml);
+                    },
+                    error: function(xhr) {
+                        var errors = xhr.responseJSON.errors;
+                        $('#titleError').text(errors.title && errors.title[0]);
+                        $('#messageError').text(errors.message && errors.message[0]);
+                    }
+                });
+            } else {
+                return false;
+            }
+        });
+        // 投稿データから詳細ページへのリンクを生成
+    });
+    </script>
+</body>
 </html>
